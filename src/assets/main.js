551ff88b687703e8e417f84a5cce36ee180a6224
McLean Brown <englishgiraffe007@gmail.com>
2017-03-17 00:43:21 -0400

    Delete main.js

--- src/assets/main.js
+++
@@ -1,9 +0,0 @@
-let answer = document.getElementById('answer');
-let attempt = document.getElementById('attempt');
-
-function guess() {
-    let input = document.getElementById('user-guess');
-    //add functionality to guess function here
-}
-
-//implement new functions here
